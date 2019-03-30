7811931cc4e94cf5bc66b9adfeeb02dfe1e23aa7
Pavel Zhdanovich <swatoplus@gmail.com>
2019-03-30 23:41:56 +0300

    Initial from template

---
+++ DirCrypt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DirCrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
